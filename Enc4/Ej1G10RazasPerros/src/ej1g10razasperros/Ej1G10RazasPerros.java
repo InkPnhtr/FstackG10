f51ea4d2dbc311d74863ba15d0eb5be8b8da91a6
Nr <Nr@tbc.com>
2023-05-31 18:49:00 -0300

    Guia10E2RazPerros

--- Enc4/Ej1G10RazasPerros/src/ej1g10razasperros/Ej1G10RazasPerros.java
+++ Enc4/Ej1G10RazasPerros/src/ej1g10razasperros/Ej1G10RazasPerros.java
@@ -6,6 +6,8 @@
 package ej1g10razasperros;
 
 import java.util.ArrayList;
+import static java.util.Arrays.sort;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -27,17 +29,41 @@ public class Ej1G10RazasPerros {
         do {
             System.out.print("Ing una Raza Pero: ");
             razas.add(leer.next());
-            System.out.println("Ing Otra raza?");
+            System.out.print("Ing Otra raza? (S/N) ");
             if (leer.next().equalsIgnoreCase("N")) { // Confirmacion s/n
                 conf = false;
             }
         } while (conf);
-        
+
         Iterator it = razas.iterator();
-        while (it.hasNext()){
+        while (it.hasNext()) {
             System.out.println(it.next());
         }
+
+        boolean nhay = false;
+        System.out.print("Ing una raza p borrar:");
+        String perroBorrar = leer.next();
+        Iterator it1 = razas.iterator();
+        while (it1.hasNext()) {
+            if (it1.next().equals(perroBorrar)) {
+                it1.remove();
+                nhay = true;
+            }
+        }
+
+        System.out.println(nhay);
+        if (!nhay) {
+            System.out.println("No HAy!");
+        }
+
+        Collections.sort(razas);
+        Iterator it2 = razas.iterator();
+
+        while (it2.hasNext()) {
+            System.out.println(it2.next());
+        }
+
+        System.out.println(
+                "End");
     }
-
 }
-
