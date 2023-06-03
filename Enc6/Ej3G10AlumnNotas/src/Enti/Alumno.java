cfde5873e503fc40e91d6c6d4d89f5b4600c686e
InkPnhtr <129902479+InkPnhtr@users.noreply.github.com>
2023-06-03 03:25:10 +0000

    Ej3G10AlumnNotas

--- Enc6/Ej3G10AlumnNotas/src/Enti/Alumno.java
+++ Enc6/Ej3G10AlumnNotas/src/Enti/Alumno.java
@@ -5,10 +5,35 @@ public class Alumno {
     private ArrayList<Integer> notas = new Arraylist<3>;
     
 
+
     /*Crear una clase llamada Alumno que mantenga información sobre las notas de distintos
 alumnos. La clase Alumno tendrá como atributos, su nombre y una lista de tipo Integer
 con sus 3 notas. */
 
 
     
+
+    public Alumno(String nombre, ArrayList<Integer> notas) {
+        Nombre = nombre;
+        this.notas = notas;
+    }
+    public Alumno() {
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public ArrayList<Integer> getNotas() {
+        return notas;
+    }
+    public void setNotas(ArrayList<Integer> notas) {
+        this.notas = notas;
+    }
+
+
+
+
+
 }
