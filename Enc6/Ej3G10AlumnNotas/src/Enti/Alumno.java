6b2d08c3f8191f150065b200c6349d9e6c5d6be4
InkPnhtr <129902479+InkPnhtr@users.noreply.github.com>
2023-06-03 03:22:39 +0000

    "Ej3G10AlumnNotas"

---
+++ Enc6/Ej3G10AlumnNotas/src/Enti/Alumno.java
@@ -0,0 +1,14 @@
+package Enti;
+
+public class Alumno {
+    private String Nombre;
+    private ArrayList<Integer> notas = new Arraylist<3>;
+    
+
+    /*Crear una clase llamada Alumno que mantenga información sobre las notas de distintos
+alumnos. La clase Alumno tendrá como atributos, su nombre y una lista de tipo Integer
+con sus 3 notas. */
+
+
+    
+}
