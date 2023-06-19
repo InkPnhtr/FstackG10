fac9b443c9a07c3b6d46f9b9ba7a97bc3a5ddaa9
Nr <Nr@tbc.com>
2023-06-19 06:13:27 -0300

    EeG11 Baraja

--- G11/Enc12/Ej2G11RuletaRusa/src/Servi/JuegServi.java
+++ G11/Enc12/Ej2G11RuletaRusa/src/Servi/JuegServi.java
@@ -35,7 +35,12 @@ Pensar la lógica necesaria para realizar esto, usando los atributos de la clase
         int i = 1;
         while (b) {
             Jugador jug = new Jugador();
-            System.out.print("Ing nombre Jugador " + i + "(Max=6!): ");
+            System.out.print("Ing nombre Jugador " + i + "(Max=6 ing x para finalizar)"
+                    + ""
+                    + ""
+                    + ""
+                    + ""
+                    +  : ");
             jug.setNombre(leer.next());
             jug.setId(i);
             i++;
